fc44f4dbf42199a12de2636bbdd48fd69b2af318
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2021-01-23 22:07:31 -0300

    separando resolver de BankAccount e Client
    O resolver de Client só é chamado caso a query bankAccount solicite o campo client.

--- src/main/java/com/bank/graphql/demo/resolver/ClientResolver.java
+++ src/main/java/com/bank/graphql/demo/resolver/ClientResolver.java
@@ -2,29 +2,25 @@ package com.bank.graphql.demo.resolver;
 
 import com.bank.graphql.demo.domain.BankAccount;
 import com.bank.graphql.demo.domain.Client;
-import com.bank.graphql.demo.domain.Currency;
-import graphql.kickstart.tools.GraphQLQueryResolver;
+import graphql.kickstart.tools.GraphQLResolver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
 
 import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @Component
-public class BankAccountResolver implements GraphQLQueryResolver {
+public class ClientResolver implements GraphQLResolver<BankAccount> {
 
-    public BankAccount bankAccount(UUID id){
-        log.info("Retrieving bank account id: {}", id);
+    public Client client(BankAccount account){
+        log.info("Retrieving client info");
 
-        var client = Client.builder()
+        return Client.builder()
                 .id(UUID.randomUUID())
                 .firstName("John")
                 .middleNames(List.of("Smith"))
                 .lastName("Doe")
                 .build();
-
-        return BankAccount.builder().id(id).currency(Currency.USD).client(client).build();
     }
 }
---
+++ src/main/java/com/bank/graphql/demo/resolver/query/BankAccountResolver.java
@@ -0,0 +1,21 @@
+package com.bank.graphql.demo.resolver.query;
+
+import com.bank.graphql.demo.domain.BankAccount;
+import com.bank.graphql.demo.domain.Currency;
+import graphql.kickstart.tools.GraphQLQueryResolver;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class BankAccountResolver implements GraphQLQueryResolver {
+
+    public BankAccount bankAccount(UUID id){
+        log.info("Retrieving bank account id: {}", id);
+
+        return BankAccount.builder().id(id).currency(Currency.USD).build();
+    }
+}
