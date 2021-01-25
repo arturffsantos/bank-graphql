a3e839419ba3eb69b81327321eac14caca27b865
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2021-01-25 20:51:22 -0300

    exception handler

---
+++ src/main/java/com/bank/graphql/demo/exception/GraphqlExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.bank.graphql.demo.exception;
+
+import graphql.GraphQLException;
+import graphql.kickstart.spring.error.ThrowableGraphQLError;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Component
+public class GraphqlExceptionHandler {
+
+    @ExceptionHandler(GraphQLException.class)
+    public ThrowableGraphQLError handle(GraphQLException e){
+        return new ThrowableGraphQLError(e);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ThrowableGraphQLError handle(RuntimeException e){
+        return new ThrowableGraphQLError(e, "Internal server error");
+    }
+}
