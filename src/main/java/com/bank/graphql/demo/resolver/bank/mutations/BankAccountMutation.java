12e341708ef7ca743e7d6883fbbdccdad6098288
Artur FF Santos <35586370+arturffsantos@users.noreply.github.com>
2021-01-25 22:28:09 -0300

    mutation resolver

---
+++ src/main/java/com/bank/graphql/demo/resolver/bank/mutations/BankAccountMutation.java
@@ -0,0 +1,24 @@
+package com.bank.graphql.demo.resolver.bank.mutations;
+
+import com.bank.graphql.demo.domain.bank.BankAccount;
+import com.bank.graphql.demo.domain.bank.Currency;
+import com.bank.graphql.demo.domain.bank.input.CreateBankAccountInput;
+import graphql.kickstart.tools.GraphQLMutationResolver;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class BankAccountMutation implements GraphQLMutationResolver {
+
+    public BankAccount createBankAccount(CreateBankAccountInput input){
+        log.info("Creating new account for {}", input);
+
+        return BankAccount.builder()
+                .id(UUID.randomUUID())
+                .currency(Currency.BRL)
+                .build();
+    }
+}
